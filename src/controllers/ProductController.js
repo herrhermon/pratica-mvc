30624768fd62c5d6ce5b855790430f451ad02c45
herrhermon <hermon.almeida@outlook.com>
2022-07-16 17:10:06 -0300

    Criado as rotas para a view de visualização para cada elemento do banco de dados

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,3 +1,32 @@
+//Criado um objeto com objetos para simular um banco de dados
+const products=[
+    {
+        id:1,
+        nome:'Blusa Azul',
+        preco:89.99,
+        tamanho:"G",
+    },
+    {
+        id:2,
+        nome:'Camisa Flamengo 21/22',
+        preco:239.00,
+        tamanho:"M",
+    },
+    {
+        id:3,
+        nome:'Camisa Flamengo 20/21',
+        preco:130.00,
+        tamanho:"P",
+    },
+    {
+        id:4,
+        nome:'Camisa Brasil Copa',
+        preco:300,
+        tamanho:"G",
+    },
+]
+
+
 //Controladores são objetos.
 
 const productController ={
@@ -5,14 +34,27 @@ const productController ={
     
     //lista dos produtos. Pode retornar uma pagina ou não.
     index: (req,res)=>{
-        return res.render('products',{title:'Lista de produtos'})
+        return res.render('products',{title:'Lista de produtos',listProducts:products})
     },
 
     //mostra um produto. Pode retornar uma pagina ou não
-    show:(req,res)=>{},
+    show:(req,res)=>{
+        const{id}=req.params // Esse valor é string
+        const productResult = products.find(product =>product.id ===parseInt(id))//converte o id para inteiro embora seja um string
+        if(!productResult){
+            //res.send('Erro . Produto não encontrado')//Mostra o texto
+            res.render('error', {title:'Pagina não encontratada'})
+        }
+        return res.render('product',{title:'Visualizar Produto', product: productResult})
+
+        //Diferença entre send e render. No send mostra algo na tela, o render renderiza uma view na tela.
+
+    },
 
     //Retorna para criar produto
-    create:(req,res)=>{},
+    create:(req,res)=>{
+        return res.render('product-create',{title:'Criar Produto'})
+    },
     
     //Cria o produto. Não retorna pagina.
     store:(req,res)=>{},
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -8,6 +8,10 @@ const router = express.Router();
 //Importar o controlador
 const productController=require('../controllers/ProductController')
 
+
+router.get("/create",productController.create)
+
 router.get("/",productController.index)
+router.get("/:id",productController.show)
 
 module.exports =router;
